6acb60cbf8bacb234ea5ead134f3ff895c2dafd4
Nick0169 <wag3711@gmail.com>
2024-04-01 12:15:01 +0630

    final

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,23 +1,20 @@
 import db from "@/lib/db";
-import { buffer } from "micro";
-
-export const config = { api: { bodyParser: false } };
 
 import { stripe } from "@/lib/stripe";
-import { NextApiRequest } from "next";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export async function POST(req: NextApiRequest) {
-  const reqBuffer = await buffer(req);
+export async function POST(req: Request) {
+  const body = await req.text();
+  console.log(body, "here is body");
   const signature = headers().get("Stripe-Signature") as string;
 
   let event: Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(
-      reqBuffer,
+      body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
